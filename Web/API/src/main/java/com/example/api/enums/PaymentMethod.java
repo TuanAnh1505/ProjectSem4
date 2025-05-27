28412c3653ae63aa1c0e2529b4fddf74e4542c19
hoang07042005 <doviethoang07042005@gmail.com>
2025-05-27 12:31:03 +0700

    1

--- Web/API/src/main/java/com/example/api/enums/PaymentMethod.java
+++
@@ -1,7 +0,0 @@
-package com.example.api.enums;
-
-public enum PaymentMethod {
-    credit_card, paypal, bank_transfer, cash, momo
-}
-
-
--- Web/API/src/main/java/com/example/api/repository/EventStatusRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.api.model.EventStatus;
-
-public interface EventStatusRepository extends JpaRepository<EventStatus, Integer> {
-    EventStatus findByStatusName(String statusName);
-}
